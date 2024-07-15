e8102b30894b5e7c23adfa1e039de37d915ada6c
kang <mr_kangsir@163.com>
2024-07-15 22:04:43 +0800

    first commit

---
+++ src/components/CustomizeTable/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <el-table :data="table.data">
+    <el-table-column v-for="item in table.columns" :key="item.prop" v-bind="item"></el-table-column>
+  </el-table>
+</template>
+
+<script>
+export default {
+  name: 'CustomizeTable',
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {}
+  },
+  computed: {
+    table() {
+      return this.data
+    },
+  },
+  methods: {},
+}
+</script>
+<style scoped lang="scss"></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <CustomizeTable :data="table"></CustomizeTable>
+  </div>
+</template>
+
+<script>
+import CustomizeTable from '@/components/CustomizeTable/index.vue'
+export default {
+  name: 'HomeView',
+  components: { CustomizeTable },
+  data() {
+    return {
+      table: {
+        columns: [
+          {
+            prop: 'date',
+            label: '日期',
+          },
+          {
+            prop: 'name',
+            label: '姓名',
+          },
+          {
+            prop: 'address',
+            label: '地址',
+            columnsType: 'slot',
+          },
+        ],
+        data: [
+          {
+            date: '2016-05-02111',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1518 弄',
+          },
+          {
+            date: '2016-05-04',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1517 弄',
+          },
+          {
+            date: '2016-05-01',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1519 弄',
+          },
+          {
+            date: '2016-05-03',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1516 弄',
+          },
+        ],
+      },
+    }
+  },
+  created() {},
+  methods: {},
+}
+</script>
+<style scoped lang="scss"></style>
